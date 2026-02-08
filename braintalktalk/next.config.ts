b8e103880d7716a02c18cb943a8d51525c44a20f
GomJS <j2su0218@gmail.com>
2026-02-08 19:52:39 +0900

    Remove comments from next.config.ts

--- braintalktalk/next.config.ts
+++ braintalktalk/next.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // 빌드 시 타입 오류가 있어도 무시하고 배포함
     ignoreBuildErrors: true,
   },
   eslint: {
-    // 빌드 시 에러 체크를 무시함
     ignoreDuringBuilds: true,
   },
 };
