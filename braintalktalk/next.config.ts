da9de3b1fe184f063ed971b4b55912b40bad6675
JISUSAMA <j2su0218@gmail.com>
2026-02-19 18:43:10 +0900

    Update next.config.ts

--- braintalktalk/next.config.ts
+++ braintalktalk/next.config.ts
@@ -3,8 +3,5 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true, // 타입 에러 무시
   },
-  eslint: {
-    ignoreDuringBuilds: true, // ESLint 에러까지 무시해서 빌드 완주
-  },
 };
 export default nextConfig;
